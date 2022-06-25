41afa3477fbe8c93d6a0cc7d995f9609bd00abe4
sushilkumbhar77@gmail.com <sushilkumbhar77@gmail.com>
2022-06-25 07:42:17 +0530

    crushCss

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+require_once "CssCrush/CssCrush.php";
+echo csscrush_tag('css/styles.css');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CssCrush</title>
+    <link rel="stylesheet" href="css/styles.crush.css" media="all" />
+  </head>
+  <body>
+    <h2>Hello CrushCss!!!</h2>
+  </body>
+</html>
